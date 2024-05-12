d49a0e76e763657cd98630a9480a03f1be1304e1
Horacio Daniel Ros <horaciodrs@gamil.com>
2024-05-12 18:20:36 -0300

    feat: add control buttons for smaller width available space

--- src/ui/IconButton.ui.ts
+++ src/ui/IconButton.ui.ts
@@ -1,5 +1,5 @@
 import "./styles/button.css";
-import { Widget, WidgetTypes } from "./widget.ui";
+import { Widget, WidgetAlignTypes, WidgetTypes } from "./widget.ui";
 import { Button } from "./button.ui";
 import { Icon } from "./Icon.ui";
 import { Label } from "./label.ui";
@@ -11,11 +11,9 @@ export class IconButton extends Button {
     constructor(id: string, icon: string = "dark_mode", parent: Widget | null = null) {
         super(id, parent);
 
+        this.setAlign(WidgetAlignTypes.HORIZONTAL);
         this.icon = new Icon(id + ".icon", icon, undefined, this);
         this.label = new Label(id + ".label", undefined, this);
-
-        this.icon.setType(WidgetTypes.FREE);
-        this.label.setType(WidgetTypes.FREE);
 
         this.init();
     }
@@ -24,38 +22,49 @@ export class IconButton extends Button {
         super.init();
     }
 
+    public onlyIcon(): boolean {
+        if (this.label.getText().length > 0) return false;
+        return true;
+    }
+
     public render(): void {
         super.render();
 
-        this.label.getBody().style.position = "absolute";
-        this.icon.getBody().style.position = "absolute";
-
-        //Estaria bueno utilizar un width fixed para el icon y uno libre para el label
-        //utilizando el Type del contendor en WidgetType.FILL
-
-        const labelHeight = this.label.getBody().clientHeight;
-
-        //const iconHeight = this.icon.getBody().clientHeight;
         const iconWidth = 24;
         const padding = 5;
 
-        const startX = padding; //this.getBody().clientWidth / 2 - (iconWidth + labelWidth) / 2;
-        const startLabelX = startX + iconWidth + padding;
-        const startY = this.getH() / 2 - 24 / 2;
-        const startLabelY = this.getH() / 2 - labelHeight / 2;
+        if (this.onlyIcon()) {
+            this.icon.getBody().style.position = "absolute";
 
-        if (startX < 0 || startY < 0) {
-            setTimeout(() => {
-                this.render();
-            }, 500);
-            return;
+            const startX = this.getBody().clientWidth / 2 - iconWidth / 2;
+            const startY = this.getH() / 2 - iconWidth / 2;
+
+            this.icon.setX(startX);
+            this.icon.setY(startY);
+        } else {
+            this.label.getBody().style.position = "absolute";
+            this.icon.getBody().style.position = "absolute";
+
+            const labelHeight = this.label.getBody().clientHeight;
+
+            const startX = padding; //this.getBody().clientWidth / 2 - (iconWidth + labelWidth) / 2;
+            const startLabelX = startX + iconWidth + padding;
+            const startY = this.getH() / 2 - iconWidth / 2;
+            const startLabelY = this.getH() / 2 - labelHeight / 2;
+
+            if (startX < 0 || startY < 0) {
+                setTimeout(() => {
+                    this.render();
+                }, 500);
+                return;
+            }
+
+            this.icon.setX(startX);
+            this.label.setX(startLabelX + padding);
+
+            this.icon.setY(startY);
+            this.label.setY(startLabelY);
         }
-
-        this.icon.setX(startX);
-        this.label.setX(startLabelX + padding);
-
-        this.icon.setY(startY);
-        this.label.setY(startLabelY);
     }
 
     public setText(text: string): void {
--- src/ui/toolbar.ui.ts
+++ src/ui/toolbar.ui.ts
@@ -1,13 +1,19 @@
 import { IWidget } from "src/interfaces/widget.interface";
 import { Widget, WidgetTypes } from "./widget.ui";
+import { IconButton } from "./IconButton.ui";
 
 export type ToolbarOrientationTypes = "horizontal" | "vertical";
 const TOOLBAR_SIZE = 40;
+const TOOLBAR_BUTTON_SIZE = 20;
 
 export class Toolbar extends Widget {
     orientation: ToolbarOrientationTypes;
     items: Map<string, IWidget>;
     size: number; //Indica el alto o ancho de la toolbar.
+
+    itemsContainer: Widget;
+    btnLeft: IconButton;
+    btnRight: IconButton;
 
     constructor(id: string, parent: Widget | null = null, orientationType: ToolbarOrientationTypes = "horizontal") {
         super(id, "div", parent);
@@ -15,7 +21,41 @@ export class Toolbar extends Widget {
         this.size = TOOLBAR_SIZE;
         this.items = new Map<string, IWidget>();
 
+        this.itemsContainer = new Widget(this.id + ".itemsContainer", "div", this);
+        this.itemsContainer.setType(WidgetTypes.CUSTOM);
+        this.itemsContainer.getBody().style.position = "absolute";
+        this.itemsContainer.getBody().style.overflow = "hidden";
+
         this.setType(WidgetTypes.FILL);
+        this.getBody().style.overflow = "hidden";
+
+        this.btnLeft = new IconButton(this.id + ".btnLeft", "arrow_left");
+        this.btnLeft.setType(WidgetTypes.CUSTOM);
+        this.btnLeft.getBody().style.position = "absolute";
+        this.btnLeft.setW(TOOLBAR_BUTTON_SIZE);
+        this.btnLeft.setH(TOOLBAR_SIZE);
+        this.addChild(this.btnLeft);
+
+        this.btnRight = new IconButton(this.id + ".btnRight", "arrow_right");
+        this.btnRight.setType(WidgetTypes.CUSTOM);
+        this.btnRight.getBody().style.position = "absolute";
+        this.btnRight.setW(TOOLBAR_BUTTON_SIZE);
+        this.btnRight.setH(TOOLBAR_SIZE);
+        this.addChild(this.btnRight);
+
+        this.btnLeft.subscribe({
+            event: "click",
+            then: () => {
+                this.itemsContainer.getBody().scrollLeft -= TOOLBAR_SIZE;
+            },
+        });
+
+        this.btnRight.subscribe({
+            event: "click",
+            then: () => {
+                this.itemsContainer.getBody().scrollLeft += TOOLBAR_SIZE;
+            },
+        });
     }
 
     /**
@@ -40,7 +80,7 @@ export class Toolbar extends Widget {
      * @param {IWidget} widget - The widget to be added.
      */
     addItem(id: string, widget: IWidget) {
-        widget.setParent(this);
+        widget.setParent(this.itemsContainer);
 
         widget.setType(WidgetTypes.CUSTOM);
 
@@ -57,8 +97,41 @@ export class Toolbar extends Widget {
         super.init();
     }
 
+    public getFullSize(): number {
+        //Devuelve el tamaño que tiene la toolbar para mostrar todos los botones.
+        let size = 0;
+        for (const item of this.items.values()) {
+            size += item.getW();
+        }
+        return size;
+    }
+
     public render(): void {
+        const fullSize = this.getFullSize();
+        const availableSize = this.getW();
+
+        if (fullSize > availableSize) {
+            this.btnLeft.setVisible(true);
+            this.btnRight.setVisible(true);
+
+            this.itemsContainer.setY(0);
+            this.itemsContainer.setX(TOOLBAR_BUTTON_SIZE);
+            this.itemsContainer.setW(availableSize - TOOLBAR_BUTTON_SIZE * 2);
+            this.itemsContainer.setH(TOOLBAR_SIZE);
+
+            this.btnLeft.setX(0);
+            this.btnRight.setX(availableSize - TOOLBAR_BUTTON_SIZE);
+        } else {
+            this.btnLeft.setVisible(false);
+            this.btnRight.setVisible(false);
+            this.itemsContainer.setY(0);
+            this.itemsContainer.setX(0);
+            this.itemsContainer.setW(fullSize);
+            this.itemsContainer.setH(TOOLBAR_SIZE);
+        }
+
         let currentPosition: number = 0;
+
         for (const item of this.items.values()) {
             if (this.orientation === "vertical") {
                 item.setY(currentPosition);
