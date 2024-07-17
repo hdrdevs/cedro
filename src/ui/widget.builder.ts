f6e2a71dcad960ccb5e83d21bd432ee2d9d5fec5
Horacio Daniel Ros <horaciodrs@gmail.com>
2024-07-17 17:26:24 -0300

    fix normalizeWidget

--- src/ui/widget.builder.ts
+++ src/ui/widget.builder.ts
@@ -165,12 +165,27 @@ export function createWidget(
 }
 
 export function normalizeWidget(widget: any, props: WidgetProps) {
-    if (widget) {
+    if (props.fixedSize !== undefined) {
         widget.setAttribute("w-fixed-size", props.fixedSize);
+    }
+
+    if (props.padding !== undefined) {
         widget.setAttribute("w-padding", props.padding);
+    }
+
+    if (props.type !== undefined) {
         widget.setAttribute("w-type", props.type);
+    }
+
+    if (props.hidden !== undefined) {
         widget.setAttribute("w-hidden", props.hidden);
+    }
+
+    if (props.classNames !== undefined) {
         widget.setAttribute("w-classes", props.classNames);
+    }
+
+    if (props.orientation !== undefined) {
         widget.setAttribute("w-orientation", props.orientation);
     }
 
