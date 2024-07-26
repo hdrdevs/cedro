564620451ebe3ed53326ec6d554116f89fd75731
Horacio Daniel Ros <horaciodrs@gmail.com>
2024-07-26 20:23:42 -0300

    .

--- src/working/demo/widget-gallery/buttons/index.tsx
+++ src/working/demo/widget-gallery/buttons/index.tsx
@@ -12,98 +12,19 @@ export default createWidget(
             <WButton id="btn3" text="Success | Text" variant="text" color="success" />
             <WButton id="btn4" text="Info | Outlined" variant="text" color="info" />
             <WButton id="btn5" text="Error | Text" variant="text" color="error" />
-            <WButton
-                id="btn6"
-                text="Warning | Text"
-                variant="text"
-                color="warning"
-                fixedSize={50}
-            />
-            <WButton
-                id="btn7"
-                text="Primary | Contained"
-                variant="contained"
-                color="primary"
-                fixedSize={50}
-            />
-            <WButton
-                id="btn8"
-                text="Secondary | Contained"
-                variant="contained"
-                color="secondary"
-                fixedSize={50}
-            />
-            <WButton
-                id="btn9"
-                text="Success | Contained"
-                variant="contained"
-                color="success"
-                fixedSize={50}
-            />
-            <WButton
-                id="btn10"
-                text="Info | Contained"
-                variant="contained"
-                color="info"
-                fixedSize={50}
-            />
-            <WButton
-                id="btn11"
-                text="Error | Contained"
-                variant="contained"
-                color="error"
-                fixedSize={50}
-            />
-            <WButton
-                id="btn12"
-                text="Warning | Contained"
-                variant="contained"
-                color="warning"
-                fixedSize={50}
-            />
-
-            <WButton
-                id="btn13"
-                text="Primary | Outlined"
-                variant="outlined"
-                color="primary"
-                fixedSize={50}
-            />
-            <WButton
-                id="btn14"
-                text="Secondary | Outlined"
-                variant="outlined"
-                color="secondary"
-                fixedSize={50}
-            />
-            <WButton
-                id="btn15"
-                text="Success | Outlined"
-                variant="outlined"
-                color="success"
-                fixedSize={50}
-            />
-            <WButton
-                id="btn16"
-                text="Info | Outlined"
-                variant="outlined"
-                color="info"
-                fixedSize={50}
-            />
-            <WButton
-                id="btn17"
-                text="Error | Outlined"
-                variant="outlined"
-                color="error"
-                fixedSize={50}
-            />
-            <WButton
-                id="btn18"
-                text="Warning | Outlined"
-                variant="outlined"
-                color="warning"
-                fixedSize={50}
-            />
+            <WButton id="btn6" text="Warning | Text" variant="text" color="warning" />
+            <WButton id="btn7" text="Primary | Contained" variant="contained" color="primary" />
+            <WButton id="btn8" text="Secondary | Contained" variant="contained" color="secondary" />
+            <WButton id="btn9" text="Success | Contained" variant="contained" color="success" />
+            <WButton id="btn10" text="Info | Contained" variant="contained" color="info" />
+            <WButton id="btn11" text="Error | Contained" variant="contained" color="error" />
+            <WButton id="btn12" text="Warning | Contained" variant="contained" color="warning" />
+            <WButton id="btn13" text="Primary | Outlined" variant="outlined" color="primary" />
+            <WButton id="btn14" text="Secondary | Outlined" variant="outlined" color="secondary" />
+            <WButton id="btn15" text="Success | Outlined" variant="outlined" color="success" />
+            <WButton id="btn16" text="Info | Outlined" variant="outlined" color="info" />
+            <WButton id="btn17" text="Error | Outlined" variant="outlined" color="error" />
+            <WButton id="btn18" text="Warning | Outlined" variant="outlined" color="warning" />
         </WContainer>
     </Layout>
 );
